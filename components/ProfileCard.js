2e76f30adde79e42d0f0c9ac7bd22b7d5e060d01
Rebotics Technologies <reboticstechnologies@gmail.com>
2025-12-27 15:41:36 +0530

    Delete components directory

--- components/ProfileCard.js
+++
@@ -1,9 +0,0 @@
-export default function ProfileCard({ user }) {
-  return (
-    <div>
-      <img src="https://i.pravatar.cc/120" />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-    </div>
-  );
-}
